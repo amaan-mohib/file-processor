6570c558c3eb0ce775eac524b67e55fd31f80dad
amaan-mohib <amaan.mohib@hotmail.com>
2025-12-09 21:26:50 +0530

    feat: add file import and export for queries

--- client/src/components/dashboard/query-card.tsx
+++ client/src/components/dashboard/query-card.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import {
   Card,
   CardAction,
@@ -12,7 +12,7 @@ import { Button } from "../ui/button";
 import useStore from "@/lib/store/useStore";
 import { uploadAndRun } from "@/lib/api/dashboard";
 import { toast } from "sonner";
-import { HelpCircle } from "lucide-react";
+import { Download, HelpCircle } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 interface QueryCardProps {}
@@ -27,6 +27,8 @@ const QueryCard: React.FC<QueryCardProps> = () => {
     setIsLoading,
   } = useStore((state) => state.dashboard);
   const getRecentJobs = useStore((state) => state.recentJobs.getRecentJobs);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputKey, setInputKey] = useState(0);
 
   const fileLen = useMemo(() => {
     let len = 0;
@@ -51,6 +53,19 @@ const QueryCard: React.FC<QueryCardProps> = () => {
     }
   };
 
+  const handleImportClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const handleImport = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setQuery(new TextDecoder().decode(await e.target.files[0].arrayBuffer()));
+      setInputKey((prev) => prev + 1);
+    }
+  };
+
   return (
     <Card className="flex-1">
       <CardHeader>
@@ -59,21 +74,35 @@ const QueryCard: React.FC<QueryCardProps> = () => {
           Queries that need to be performed on the uploaded assets
         </CardDescription>
         <CardAction>
-          <Tooltip>
-            <TooltipTrigger>
-              <a
-                href="https://github.com/amaan-mohib/file-processor/blob/main/FILEQUERY_SYNTAX.md"
-                target="_blank">
-                <Button
-                  variant={"secondary"}
-                  size={"icon"}
-                  className="cursor-pointer">
-                  <HelpCircle />
-                </Button>
-              </a>
-            </TooltipTrigger>
-            <TooltipContent side="left">Help</TooltipContent>
-          </Tooltip>
+          <div className="flex gap-2">
+            <Button variant={"outline"} onClick={handleImportClick}>
+              <Download />
+              Import
+            </Button>
+            <input
+              ref={inputRef}
+              key={inputKey}
+              type="file"
+              accept=".dpq"
+              className="hidden"
+              onChange={handleImport}
+            />
+            <Tooltip>
+              <TooltipTrigger>
+                <a
+                  href="https://github.com/amaan-mohib/file-processor/blob/main/FILEQUERY_SYNTAX.md"
+                  target="_blank">
+                  <Button
+                    variant={"secondary"}
+                    size={"icon"}
+                    className="cursor-pointer">
+                    <HelpCircle />
+                  </Button>
+                </a>
+              </TooltipTrigger>
+              <TooltipContent side="bottom">Help</TooltipContent>
+            </Tooltip>
+          </div>
         </CardAction>
       </CardHeader>
       <CardContent className="flex flex-col gap-4 flex-1">
--- client/src/routes/_authenticated/jobs/$jobId.tsx
+++ client/src/routes/_authenticated/jobs/$jobId.tsx
@@ -1,11 +1,20 @@
 import JobCard from "@/components/dashboard/job-card";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { getJob } from "@/lib/api/jobs";
 import useStore from "@/lib/store/useStore";
 import type { IFile, IJob } from "@/lib/types";
 import { formatFullDate } from "@/lib/utils";
 import { createFileRoute } from "@tanstack/react-router";
+import { Upload } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/_authenticated/jobs/$jobId")({
@@ -28,6 +37,19 @@ function RouteComponent() {
     });
   }, [jobId]);
 
+  const exportQuery = () => {
+    if (!data?.job || !data?.job.query) {
+      return;
+    }
+    const blob = new Blob([data.job.query], { type: "text/plain" });
+    const elem = window.document.createElement("a");
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = `${data.job.jobKey}-query.dpq`;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  };
+
   if (!data) {
     return <div>Loading...</div>;
   }
@@ -40,6 +62,13 @@ function RouteComponent() {
       <Card>
         <CardHeader>
           <CardTitle>Query</CardTitle>
+          <CardDescription>Set of queries ran on the file</CardDescription>
+          <CardAction>
+            <Button variant={"secondary"} onClick={exportQuery}>
+              <Upload />
+              Export
+            </Button>
+          </CardAction>
         </CardHeader>
         <CardContent>
           <Textarea rows={20} readOnly value={job.query} />
